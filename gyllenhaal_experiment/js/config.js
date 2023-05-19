5abacf98daf5bb9cdb0dbd2155a7fd78a6bb5c6a
MatthieuFra <93255376+MatthieuFra@users.noreply.github.com>
2023-05-19 15:58:15 +0200

    update

--- gyllenhaal_experiment/js/config.js
+++ gyllenhaal_experiment/js/config.js
@@ -9,6 +9,21 @@ https://matthieufra.github.io/cultural_evolution_gyllenhaal_experiment/gyllenhaa
 
 */
 
+INSTRUCTION1 = `
+<p>Welcome to this transmission chain experiment! This study explores the difficulty of spelling specific names.</p>
+
+<p>You will be asked to spell the last name of a celebrity appearing on the screen. You will be shown a picture of the celebrity and an audio of his*her name.</p> 
+<p>You will also be shown how the previous participant spelt it!</p> 
+
+<p>Good luck!</p>`,
+
+INSTRUCTION2 = `
+<p>Welcome to this transmission chain experiment! This study explores the difficulty of spelling specific names.</p>
+
+<p>You will be asked to spell the last name of a celebrity appearing on the screen. You will be shown a picture of the celebrity and an audio of his*her name.</p> 
+
+<p>Good luck!</p>`,
+
 //The link of the image used
 IMAGE = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.standard.co.uk%2Fs3fs-public%2Fthumbnails%2Fimage%2F2019%2F11%2F21%2F13%2Frobert-pattinson-211119.jpg&f=1&nofb=1&ipt=8d2d5eb07c4e8b3a05dfd20be3abcbde60e90a6f951c1064945096ffe91c5219&ipo=images';
 
